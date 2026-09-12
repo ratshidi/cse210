46f5d06b71ae72a60e01485ded3b74f5bf2f528f
ratshidi <pratshidi@byupathway.edu>
2026-09-12 23:51:34 +0200

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101)
+        int magicNumber = randomGenerator.Next(1, 101);
 
         int guess = -1;
 
